16dff1318c0c4472dd116a33f5b03b638c9e2f85
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-23 22:27:30 +0300

    cache featured books

--- lib/features/home/data/data_sources/home_remote_data_source/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source/home_remote_data_source.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 
+import '../../../../../core/functions/save_books_into_hive.dart';
 import '../../models/book_model/book_model.dart';
 
 abstract class HomeRemoteDataSource {
@@ -19,11 +21,15 @@ class HomeRemoteDataSourceImp implements HomeRemoteDataSource {
     var jsonData = await apiService.get(
         endPoint: 'volumes?q=programming&Filtering=free-ebooks');
     List<BookEntity> books = parseJsonStringToBookList(jsonData);
+    saveBooksIntoHiveBox(
+      books: books,
+      boxName: kFeaturedBox,
+    );
     return books;
   }
 
   @override
-  Future<List<BookEntity>> fetchNewestBooks() async{
+  Future<List<BookEntity>> fetchNewestBooks() async {
     var jsonData = await apiService.get(
         endPoint: 'volumes?q=programming&Filtering=free-ebooks&Sorting=newest');
     List<BookEntity> books = parseJsonStringToBookList(jsonData);
